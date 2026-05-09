79ba3727001c84c526600b68575563dd22e32cb9
SRCreator <85744292+onu032001@users.noreply.github.com>
2026-05-09 18:41:02 +0900

    add textarea to codearea conversion for clarity

--- codearea.js
+++ codearea.js
@@ -3,7 +3,7 @@ class Codearea extends HTMLElement {
         super();
         this.textarea = document.createElement('textarea');
         this.appendChild(this.textarea);
-        this.value = '';
+        this._value = '';
         this.selectionStart = 0;
         this.selectionEnd = 0;
         this.textarea.addEventListener('keydown', (event) => {
@@ -22,21 +22,30 @@ class Codearea extends HTMLElement {
         this.textarea.style.fontFamily = 'monospace';
         this.textarea.style.width = '350px';
         this.textarea.style.height = '250px';
+
+        this.updateState();
+    }
+    get value() {
+        return this._value;
+    }
+    set value(valuePar) {
+        this._value = valuePar;
+        this.updateState();
     }
     getLineNumber(index) {
-        return this.value.slice(0, index).split('\n').length - 1;
+        return this._value.slice(0, index).split('\n').length - 1;
     }
     getLastSpaces(index) {
         const lineNumber = this.getLineNumber(index);
-        const content = this.value.split('\n')[lineNumber];
+        const content = this._value.split('\n')[lineNumber];
         return content.match(/^\s*/g);
     }
     insertText(text) {
         const start = this.selectionStart,
             end = this.selectionEnd,
-            value = this.value;
+            value = this._value;
         
-        this.value = value.substring(0, start) + text + value.substring(end);
+        this._value = value.substring(0, start) + text + value.substring(end);
         this.selectionStart = this.selectionEnd = start + text.length;
     }
     insertTab(event) {
@@ -55,14 +64,14 @@ class Codearea extends HTMLElement {
         const lineNumber_start = this.getLineNumber(start);
         const lineNumber_end = this.getLineNumber(end);
 
-        const splitted = this.value.split('\n');
+        const splitted = this._value.split('\n');
         for (let index = lineNumber_start; index <= lineNumber_end; index++) {
             splitted[index] = '  ' + splitted[index];
         }
         const joined = splitted.join('\n');
 
-        const original = this.value;
-        this.value = joined;
+        const original = this._value;
+        this._value = joined;
         this.selectionEnd += joined.length - original.length;
     }
     outdent(event) {
@@ -72,26 +81,34 @@ class Codearea extends HTMLElement {
         const lineNumber_start = this.getLineNumber(start);
         const lineNumber_end = this.getLineNumber(end);
 
-        const splitted = this.value.split('\n');
+        const splitted = this._value.split('\n');
         for (let index = lineNumber_start; index <= lineNumber_end; index++) {
             splitted[index] = splitted[index].replace(/^\s{2}/, '');
         }
         const joined = splitted.join('\n');
 
-        const original = this.value;
-        this.value = joined;
+        const original = this._value;
+        this._value = joined;
         this.selectionEnd += joined.length - original.length;
     }
     inputHandler() {
-        this.value = this.textarea.value;
+        this._value = this.textarea.value;
         this.selectionStart = this.textarea.selectionStart;
         this.selectionEnd = this.textarea.selectionEnd;
     }
     updateState() {
-        this.textarea.value = this.value;
+        this.textarea.value = this._value;
         this.textarea.selectionStart = this.selectionStart;
         this.textarea.selectionEnd = this.selectionEnd;
     }
 }
 
 customElements.define('code-area', Codearea);
+
+function changeTextareaToCodearea(textarea) {
+    const codearea = document.createElement('code-area');
+    codearea.value = textarea.value;
+    codearea.selectionStart = textarea.selectionStart;
+    codearea.selectionEnd = textarea.selectionEnd;
+    textarea.parentElement.replaceChild(codearea, textarea);
+}
